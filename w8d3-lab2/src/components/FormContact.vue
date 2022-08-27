c5ef950624fb3f65c66e4a4e946d6e50c0e02463
Mireia Bertran <mbertran9@gmail.com>
2022-08-27 18:48:42 +0200

    no funciona

--- w8d3-lab2/src/components/FormContact.vue
+++ w8d3-lab2/src/components/FormContact.vue
@@ -1,5 +1,10 @@
 <template>
   
+<input v-model="name" placeholder="Nombre"><br>
+<input v-model="mail" placeholder="e-mail">
+<p>mi nombre és: {{ name }}</p>  
+<p>mi e-mail és: {{ mail }}</p>  
+
 </template>
 
 <script>
