54f702b81c51de4666b3b6124dad3d4f005fabe7
Mireia Bertran <mbertran9@gmail.com>
2022-09-01 17:59:52 +0200

    4-5

--- w8d3-lab2/src/App.vue
+++ w8d3-lab2/src/App.vue
@@ -1,27 +1,39 @@
 <template>
-<div>
-<NavBar></NavBar>
-<p>{{2 + 2}}</p>
-<FormContact></FormContact>
-<FooterMain></FooterMain>
-</div>
+  <div>
+    <NavBar></NavBar>
+    <p>{{ 2 + 2 }}</p>
+    <FormContact></FormContact>
+    <hr />
+    <div v-if="DayOfClass === `true`">
+      Today you have class in Ironhack, connect your computer!!!
+    </div>
+    <div v-else>Day off, go for a ride</div>
+    <hr />
+    <PostBloc></PostBloc>
+    <FooterMain></FooterMain>
+  </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
-import FooterMain from './components/FooterMain.vue'
-import FormContact from './components/FormContact.vue'
+import NavBar from "./components/NavBar.vue";
+import FooterMain from "./components/FooterMain.vue";
+import FormContact from "./components/FormContact.vue";
+import PostBloc from "./components/PostBloc.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      DayOfClass: "true",
+    };
+  },
   components: {
     NavBar,
     FooterMain,
     FormContact,
-  }
-}
+    PostBloc
+},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ w8d3-lab2/src/components/PostBloc.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <div v-for="post in posts" :key="post.uuid">
+    <h3>{{ post.title }}</h3>
+    <h4>{{ post.description }}</h4>
+    <p>{{ post.content }}</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "PostBloc",
+  data() {
+    return {
+      posts: [
+        {
+          uuid: "1",
+          title: "post número uno",
+          description:
+            "Descripción uno, Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+          content:
+            "Contenido uno, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+        {
+          uuid: "2",
+          title: "post número dos",
+          description:
+            "Descripción dos, Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+          content:
+            "Contenido dos, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+        {
+          uuid: "3",
+          title: "post número tres",
+          description:
+            "Descripción tres, Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+          content:
+            "Contenido tres, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+      ],
+    };
+  },
+  components: {},
+};
+</script>
+
+<style></style>
